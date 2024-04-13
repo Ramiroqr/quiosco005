8d43f298907a2629cab11b5f69bc9ed75cfa0d11
Ramiroqr <ramiroquisper@gmail.com>
2024-04-13 12:37:35 -0500

    first comit

---
+++ components/order/OrderSummary.tsx
@@ -0,0 +1,8 @@
+
+export default function OrderSummary() {
+  return (
+    <aside className="md:w-64 lg:h-screen lg:w-96 lg:overflow-y-scroll p-5">
+        <h1 className="text-4xl text-center font-black">Mi Pedido</h1>
+    </aside>
+  )
+}
