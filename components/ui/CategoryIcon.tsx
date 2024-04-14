50178f1f842d16eb5b400c8042beb67709d6a31b
Ramiroqr <ramiroquisper@gmail.com>
2024-04-14 13:35:54 -0500

    Showing categories

--- components/ui/CategoryIcon.tsx
+++ components/ui/CategoryIcon.tsx
@@ -1,4 +1,6 @@
 import { Category } from "@prisma/client"
+import Image from "next/image"
+import Link from "next/link"
 
 type CategoryIconProps = {
     category: Category
@@ -6,6 +8,21 @@ type CategoryIconProps = {
 
 export default function CategoryIcon({category}: CategoryIconProps) {
   return (
-    <div>{category.name}</div>
+    <Link
+        className={`flex items-center gap-4 w-full border-t border-gray-200 p-3 last-of-type:border-b`}
+        href={`/order/${category.slug}`}
+    >
+        <div className="w-16 h-16 relative">
+            <Image 
+                fill
+                src={`/icon_${category.slug}.svg`} 
+                alt="Imagen Categoria"
+            />
+        </div>
+
+        <p
+            className="text-xl font-bold"
+        >{category.name}</p>
+    </Link>
   )
 }
